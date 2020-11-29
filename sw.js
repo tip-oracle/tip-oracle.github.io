895d701dada793ac51a7a0e3f1454c12193848b8
Stanislav <quah.fy95@gmail.com>
2020-11-29 15:48:24 +0000

    Deploying at Sun 29 Nov 15:48:24 GMT 2020

--- sw.js
+++ sw.js
@@ -14,11 +14,11 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = "gzrcxn3D7njLNUz4sOwAdCrShjQusRcx";
+const PRECACHE = "yMFFa60fEALTel1wUL4RMM0Tgen1BOCs";
 // const RUNTIME  = "runtime"
 
 // A list of local resources we always want to be cached.
-const PRECACHE_URLS = ["/index.html", "/static/js/2.07693b27.chunk.js", "/static/js/3.c518c7c8.chunk.js", "/static/js/main.e69b04d8.chunk.js", "/static/js/runtime-main.9f75709c.js", "/static/css/2.db3a489f.chunk.css", "/static/css/main.377ffcb0.chunk.css", ];
+const PRECACHE_URLS = ["/index.html", "/static/js/2.07693b27.chunk.js", "/static/js/main.3933e014.chunk.js", "/static/js/3.c518c7c8.chunk.js", "/static/js/runtime-main.9f75709c.js", "/static/css/2.db3a489f.chunk.css", "/static/css/main.377ffcb0.chunk.css", ];
 
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
